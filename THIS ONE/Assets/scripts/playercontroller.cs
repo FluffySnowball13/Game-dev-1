dbfc8536d8d252878952c29035ae0ffe4984aeb2
Fluffy <alexis.c964@stu.nebo.edu>
2021-09-03 14:47:57 -0600

    movement and physics

--- THIS ONE/Assets/scripts/playercontroller.cs
+++ THIS ONE/Assets/scripts/playercontroller.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class playercontroller : MonoBehaviour
 {
     public float speed; 
-    public float turnspeed; 
+    public float turnSpeed; 
+    
+    private float hInput;
+    private float vInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,13 @@ public class playercontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        // Get horizontal and vertical inputs
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+        // Move Tank foward and Back
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * vInput);
+        // Rotate Tank left and right
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * hInput);
     }
 }
