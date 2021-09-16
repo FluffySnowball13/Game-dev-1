bc75419930167591815c925782181d0d7c7493b5
Fluffy <alexis.c964@stu.nebo.edu>
2021-09-16 14:30:56 -0600

    script update

--- dieases/Assets/scripts/Player.cs
+++ dieases/Assets/scripts/Player.cs
@@ -2,21 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Playercontroller : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    public float speed = 2.0f;
-    public float turnspeed = 10.0f;
+    public float speed = 25.0f;
+    public float turnSpeed = 50.0f;
     private float hInput;
     private float vInput;
 
  // Update is called once per frame
     void Update()
     {
-        hInput = Input.GetAxis("Horizontal")
-        vInput = Input.GetAxis("Vertical")
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
  // makes the p rotate left and right
-        transform.Rotate(Vector3.up, * turnSpeed * Time.deltaTime * hInput);
+        transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime * hInput);
  // makes the p move foward and back
-        transform.Translate(Vector3.foward * speed * Time.deltaTime * vInput);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * vInput);
     }
 }
