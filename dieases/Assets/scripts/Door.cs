0d031f9e3f78ca2f545f7d401adba06240cb8229
Fluffy <alexis.c964@stu.nebo.edu>
2021-10-01 14:19:04 -0600

    door and key

---
+++ dieases/Assets/scripts/Door.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public GameManager gameManager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D)
+    {
+        if(other.CompareTag("player") && gameManager.hasKey)
+        {
+            print("you have unlocked the door!"); 
+        }
+    }
+}
