bc994ec865d029d6f1f70fe96252ef15b6e59e2d
Fluffy <alexis.c964@stu.nebo.edu>
2021-10-19 14:18:24 -0600

    flag time

--- protype 3 firstparsom/Assets/script/Weapon.cs
+++ protype 3 firstparsom/Assets/script/Weapon.cs
@@ -50,15 +50,4 @@ public class Weapon : MonoBehaviour
         // set the velocity
         bullet.GetComponent<Rigidbody>().velocity = muzzle.forward * bulletSpeed;
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
