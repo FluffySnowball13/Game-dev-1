05091cd316ba0a9b877163cb587fc409b5159b19
Fluffy <alexis.c964@stu.nebo.edu>
2021-10-28 14:20:35 -0600

    W.I.P enemy script. Added some obstacles.

---
+++ protype 3 firstparsom/Assets/script/Enemy.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using System.Linq;
+
+public class Enemy : MonoBehaviour
+{
+
+    [Header("Stats")]
+    public int curHP;
+    public int maxHP;
+    public int scoretoGive;
+
+    [Header("Movement")]
+    public float moveSpeed;
+    public float attackRange;
+
+    public float yPathOffset;
+
+
+    private List<Vector3> path;
+
+    private Weapon weapon;
+    private GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Gather the Components
+        weapon = GetComponents<Weapon>();
+        target = FindObjectOfType<PlayerController>().gameObject;
+
+        InvokeRepeating("UpdatePath", 0.0f, 0.5f);
+    }
+
+    void UpdatePath ()
+    {
+        // Calculate a path to the target
+        NavMeshPath navMeshPath = new navMeshPath();
+        NavMesh.CalculatePath(transform.position, target.transform.position, navMeshPath.AllAreas, navMeshPath);
+
+        //Save path as a list
+        path = navMesh.corners.ToList();
+    }
+
+    void ChaseTarget()
+    {
+        if(path.Count == 0)
+        return;
+
+        // move toward the closest path
+        transform.position = Vector3.MoveTowards(transform.position, path[0] + new Vector3 (0, yPathOffset, 0), moveSpeed * Time.deltaTime);
+
+       if(transform.position == path[0] + new Vector3(0, yPathOffset, 0))
+        path.RemoveAt(0); 
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
