62ed76fda93c053093e907143b9586551ec4207a
Fluffy <alexis.c964@stu.nebo.edu>
2021-11-11 14:20:49 -0700

    Ammo and health

---
+++ protype 3 firstparsom/Assets/script/Pickup.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum PickupType
+{
+    Health,
+    Ammo
+}
+public class Pickup : MonoBehaviour
+{
+    public  PickupType type;
+    public int value;
+
+    [Header ("Bobbing Anim")]
+    public float rotationSpeed;
+    public float bobSpeed;
+    public float bobHeight;
+
+    private Vector3
+    private bool bobbingUp;
+
+    void start()
+    {
+        //Set start pos
+        startPos = tamsform.position;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+ 
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            PlayerController player = other.GetComponent<PlayerController>();
+
+            switch(type)
+            {
+                case PickupType.Health:
+                player.GiveHealth(value);
+                break; 
+
+                case PickupType.Ammo:
+                player.GiveAmmo(value);
+                break;
+            }
+
+            Destroy(gameObject);
+
+         
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotating
+        transform.Roatate(Vector3.up, rotationSpeed + Time.deltaTime);
+
+        //
+        Vector3 offset = (bobbingUp == true ? new Vector3(0,bobHeight /2, 0)) : new Vector3(0, -bobHeight /2, 0);
+        transform.position = Vector3.MoveTowards(transform.position, startpos + offset, bobSpeed + Time.deltaTime);
+        
+        if(transform.position == startpPos + offset)
+            bobbingUp = !bobbingUp;
+    }
+}
